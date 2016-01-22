b56daa281541913341b1ddfb4c9e1f0c236b8c6b
Michał Papierski <michal@papierski.net>
2016-01-22 19:56:20 +0100

    Improved announcement extraction.

--- edux.py
+++ edux.py
@@ -14,7 +14,10 @@ def extract_courses(fileobj):
     courses = bs.select('#ctl00_ContentPlaceHolder1_grdKursy_ctl00 tbody')[0]
     for tr in courses.select('tr'):
         a = tr.select('a')[0]
-        yield (a['title'], a['href'])
+        (course_id, ) = re.findall(
+            r'^https://edux\.pjwstk\.edu\.pl/req\.aspx\?id=(\d+)$', a['href'])
+        course_id = int(course_id)
+        yield (course_id, a['title'], a['href'])
 
 
 def extract_announcements(fileobj):
@@ -25,21 +28,29 @@ def extract_announcements(fileobj):
     bs = BeautifulSoup(fileobj.read(), 'html.parser')
     announcements = bs.select(
         '#ctl00_ContentPlaceHolder1_grdOgloszenia_ctl00 tbody')[0]
+
+    result = []
+
     for announcement in announcements.select('tr'):
         (timestamp, message, ) = announcement.select('td')
         timestamp = datetime.strptime(timestamp.text, '%Y-%m-%d').date()
         message = re.sub(r'\s+', ' ', message.text)
-        yield message
+        result.append((timestamp, message))
+    return reversed(result)
 
 
 def main():
     p = '/Users/michal/Downloads/edux'
 
     with open(os.path.join(p, 'Premain.aspx.html')) as fileobj:
-        extract_courses(fileobj)
+        for (course_id, title, href) in extract_courses(fileobj):
+            print (course_id, title, href)
 
     with open(os.path.join(p, 'Announcements.aspx.html')) as fileobj:
-        extract_announcements(fileobj)
+        for (timestamp, message) in extract_announcements(fileobj):
+            print '---'
+            print timestamp
+            print message
 
 
 if __name__ == '__main__':
