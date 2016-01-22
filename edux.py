f389f937bc8c48cf5ac7389aee76ee3bdc021dc5
Michał Papierski <michal@papierski.net>
2016-01-22 20:36:04 +0100

    Navigate through the website.

--- edux.py
+++ edux.py
@@ -1,8 +1,38 @@
 import os
 import re
+import sys
+from StringIO import StringIO
 from datetime import datetime
 
+import requests
 from bs4 import BeautifulSoup
+
+
+s = requests.Session()
+
+
+class LoginError(Exception):
+    '''Unable to login'''
+
+
+def get_form_events(bs):
+    events = [
+        '__EVENTTARGET',
+        '__EVENTARGUMENT',
+        '__VIEWSTATE',
+        '__EVENTVALIDATION',
+        '__VIEWSTATEGENERATOR'
+    ]
+    event = {}
+    for e in events:
+        css = 'input#{0}'.format(e)
+        l = bs.select(css)
+        try:
+            event[e] = l[0]['value']
+        except IndexError:
+            pass
+
+    return event
 
 
 def extract_courses(fileobj):
@@ -15,9 +45,10 @@ def extract_courses(fileobj):
     for tr in courses.select('tr'):
         a = tr.select('a')[0]
         (course_id, ) = re.findall(
-            r'^https://edux\.pjwstk\.edu\.pl/req\.aspx\?id=(\d+)$', a['href'])
+            r'req\.aspx\?id=(\d+)$', a['href'])
         course_id = int(course_id)
-        yield (course_id, a['title'], a['href'])
+        url = 'https://edux.pjwstk.edu.pl/' + a['href']
+        yield (course_id, a['title'], url)
 
 
 def extract_announcements(fileobj):
@@ -39,8 +70,84 @@ def extract_announcements(fileobj):
     return reversed(result)
 
 
+def login(username, password):
+    '''Logs in to the edux website
+    '''
+    url = 'https://edux.pjwstk.edu.pl/Login.aspx'
+    r = s.get(url)
+    r.raise_for_status()
+    bs = BeautifulSoup(r.content, 'html.parser')
+    form = get_form_events(bs)
+    form['ctl00$ContentPlaceHolder1$Login1$LoginButton'] = 'Zaloguj'
+    form['ctl00$ContentPlaceHolder1$Login1$UserName'] = username
+    form['ctl00$ContentPlaceHolder1$Login1$Password'] = password
+    form['ctl00_RadScriptManager1_TSM'] = ''
+    r = s.post(url, data=form)
+    r.raise_for_status()
+
+    # Find error
+    bs = BeautifulSoup(r.content, 'html.parser')
+
+    try:
+        font = bs.select(
+            '#ctl00_ContentPlaceHolder1_Login1 tr td table font[color=Red]')[0]
+        error = font.text.strip()
+        if error:
+            raise LoginError(error)
+    except IndexError:
+        pass
+
+
+def get_announcements(url):
+    '''Gets all announcements
+    '''
+    r = s.get('https://edux.pjwstk.edu.pl/Announcements.aspx')
+    r.raise_for_status()
+    with open('Course.html', 'w') as fileobj:
+        fileobj.write(r.content)
+    fileobj = StringIO(r.content)
+
+    for (timestamp, message) in extract_announcements(fileobj):
+        print '---'
+        print timestamp
+        print message
+
+
+def get_courses():
+    '''Navigates to Premain
+
+    Gets all courses
+    '''
+    r = s.get('https://edux.pjwstk.edu.pl/Premain.aspx')
+    r.raise_for_status()
+    fileobj = StringIO(r.content)
+    for (course_id, name, url) in extract_courses(fileobj):
+        # Get inside the course
+        r = s.get(url)
+        r.raise_for_status()
+        try:
+            print course_id
+            get_announcements(url)
+        finally:
+            # Go back (is)
+            # r = s.get('https://edux.pjwstk.edu.pl/premain.aspx')
+            pass
+
+
 def main():
     p = '/Users/michal/Downloads/edux'
+
+    try:
+        print 'login'
+        login(os.environ['EDUX_USERNAME'], os.environ['EDUX_PASSWORD'])
+    except LoginError as e:
+        sys.stderr.write(u'LoginError({})\n'.format(e))
+    else:
+        get_courses()
+    finally:
+        print 'logout'
+
+    return
 
     with open(os.path.join(p, 'Premain.aspx.html')) as fileobj:
         for (course_id, title, href) in extract_courses(fileobj):
